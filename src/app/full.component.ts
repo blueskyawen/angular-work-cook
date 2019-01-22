075487c8c4d773cbcd03efdb085b03ba6e42b0c0
blueskyawen <bluesky.liu@163.com>
2019-01-22 20:23:44 +0800

    fix

--- src/app/full.component.ts
+++ src/app/full.component.ts
@@ -13,19 +13,16 @@ export class FullComponent implements OnInit {
     navItems: [
       {title:'基本烹饪',
         icon:'fa fa-coffee',
-        isActive:true,
         url:'/main/basic/heros',
         callback: () => {this.router.navigate(['/main/basic/heros']);}
       },
       {title:'进阶烹饪',
         icon:'fa fa-tree',
-        isActive:false,
         url:'/main/advance',
         callback: () => {this.router.navigate(['/main/advance']);}
       },
       {title:'其他',
         icon:'fa fa-smile-o',
-        isActive:false,
         url:'/main/other',
         callback: () => {this.router.navigate(['/main/other']);}
       }
@@ -37,29 +34,27 @@ export class FullComponent implements OnInit {
     navItems: [
       {title:'英雄指南',
         icon:'fa fa-coffee',
-        isActive:true,
         url:'/main/basic/heros/topHeros',
         callback: () => {this.router.navigate(['/main/basic/heros/topHeros']);},
       },
       {title:'组件与模板',
         icon:'fa fa-wifi',
-        isActive:false,
         childs: {
           place: 'down',isShow:false,
           navItems: [
-            {title:'动态组件',icon:'',isActive:false,place: 'down',
+            {title:'动态组件',icon:'',place: 'down',
               url:'/main/basic/comp/dynamic',
               callback: () => {this.router.navigate(['/main/basic/comp/dynamic']);}},
-            {title:'自定义元素',icon:'',isActive:false,place: 'down',
+            {title:'自定义元素',icon:'',place: 'down',
               url:'/main/basic/comp/customEle',
               callback: () => {this.router.navigate(['/main/basic/comp/customEle']);}},
-            {title:'属性指令',icon:'',isActive:false,place: 'down',
+            {title:'属性指令',icon:'',place: 'down',
               url:'/main/basic/comp/attrDirective',
               callback: () => {this.router.navigate(['/main/basic/comp/attrDirective']);}},
-            {title:'结构指令',icon:'',isActive:false,place: 'down',
+            {title:'结构指令',icon:'',place: 'down',
               url:'/main/basic/comp/structDirective',
               callback: () => {this.router.navigate(['/main/basic/comp/structDirective']);}},
-            {title:'管道',icon:'',isActive:false,place: 'down',
+            {title:'管道',icon:'',place: 'down',
               url:'/main/basic/comp/pipe',
               callback: () => {this.router.navigate(['/main/basic/comp/pipe']);}}
           ]
@@ -67,17 +62,16 @@ export class FullComponent implements OnInit {
       },
       {title:'表单',
         icon:'fa fa-camera',
-        isActive:false,
         childs: {
           place: 'down',isShow:false,
           navItems: [
-            {title:'模板式表单',icon:'',isActive:false,place: 'down',
+            {title:'模板式表单',icon:'',place: 'down',
               url:'/main/basic/form/temple',
               callback: () => {this.router.navigate(['/main/basic/form/temple']);}},
-            {title:'响应式表单',icon:'',isActive:false,place: 'down',
+            {title:'响应式表单',icon:'',place: 'down',
               url:'/main/basic/form/reactive',
               callback: () => {this.router.navigate(['/main/basic/form/reactive']);}},
-            {title:'动态表单',icon:'',isActive:false,place: 'down',
+            {title:'动态表单',icon:'',place: 'down',
               url:'/main/basic/form/dynamic',
               callback: () => {this.router.navigate(['/main/basic/form/dynamic']);}}
           ]
