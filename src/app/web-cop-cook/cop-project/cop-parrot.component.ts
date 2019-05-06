ee6ec0bdaaa11b48849405381c13a79b902517dd
blueskyawen <bluesky.liu@163.com>
2019-05-07 02:07:36 +0800

    add cop pro

--- src/app/web-cop-cook/cop-project/cop-parrot.component.ts
+++ src/app/web-cop-cook/cop-project/cop-parrot.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CopParrotComponent implements OnInit {
   showIt: boolean = false;
+  isDong: boolean = false;
   constructor() { }
 
   ngOnInit() {
