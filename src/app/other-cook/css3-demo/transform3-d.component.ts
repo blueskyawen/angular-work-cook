57d96a85d7b02d2ba93bdcf8382ef333b1a106e2
blueskyawen <bluesky.liu@163.com>
2019-05-23 01:47:24 +0800

    dsds

--- src/app/other-cook/css3-demo/transform3-d.component.ts
+++ src/app/other-cook/css3-demo/transform3-d.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, ElementRef, Renderer2, ViewChild } from '@angular/co
   styleUrls: ['./transform3-d.component.less']
 })
 export class Transform3DComponent implements OnInit {
-  data3d: any = {
+  data3d1: any = {
     trans: {x:'0',y:'0',z:'0'},
     scal: {x:'1',y:'1',z:'1'},
     rotat: {x:'0',y:'0',z:'1',deg:'45'},
@@ -16,6 +16,8 @@ export class Transform3DComponent implements OnInit {
     originz: {x: 50, y: 50, type: 'percentage', xx: 'left', yy: 'top'},
     transOrigin: {x: 50, y: 50, type: 'percentage', xx: 'left', yy: 'top'}
   };
+  data3d2: any;
+  data3dList: any[] = [];
   numReg1 : any = new RegExp("^[-]?\\d+$");
   numReg2 : any = new RegExp("^\\d+$");
   numReg3 : any = new RegExp("^\\d+(\\.\\d+)?$");
@@ -28,8 +30,9 @@ export class Transform3DComponent implements OnInit {
   };
   @ViewChild('translate3dout2')
       Translate3doutRef2: ElementRef;
-  selectTransform2: string = '';
-
+  selectTransform2: any = {
+    trans: '', stytez: '', visib: '', perspec: '', originz: '', peroriginz: ''
+  };
   styleOptions: any[] = [
     {label: 'flat',value: 'flat',disable: false},
     {label: 'preserve-3d',value: 'preserve-3d',disable: false}
@@ -53,60 +56,115 @@ export class Transform3DComponent implements OnInit {
   constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.data3dList.push(this.data3d1);
+    this.data3d2 = JSON.parse(JSON.stringify(this.data3d1));
   }
 
-  make3DTransformOut1(index: string) {
-    if(index === '1') {
-      this.selectTransform1.trans = '';
-      this.selectTransform1.trans = `translate3d(${this.getValue(this.data3d.trans.x,0)}px,${this.getValue(this.data3d.trans.y,0)}px ,${this.getValue(this.data3d.trans.z,0)}px) ` +
-          `scale3d(${+this.getValue(this.data3d.scal.x,2)},${+this.getValue(this.data3d.scal.y,2)},${+this.getValue(this.data3d.scal.y,2)}) ` +
-          `rotate3d(${this.getValue(this.data3d.rotat.x,3)},${this.getValue(this.data3d.rotat.y,3)},${this.getValue(this.data3d.rotat.z,3)},${this.getValue(this.data3d.rotat.deg,0)}deg)`;
-      this.renderer.setStyle(this.Translate3doutRef1.nativeElement, 'transform', this.selectTransform1.trans);
-    }
-
-    this.makePerspectiveOut(index);
-    this.makePerspectiveOriginOut(index);
-    this.makebackfaceVisibilityOut(index);
-    this.maketransformStyleOut(index);
-    this.maketransformOriginOut(index);
-  }
-
-  makePerspectiveOut(index: string) {
-    if(index === '1') {
-      this.selectTransform1.perspec = this.data3d.perspec;
-      this.renderer.setStyle(this.Translate3doutRef1.nativeElement, 'perspective', this.selectTransform1.perspec);
+  checkedChange() {
+    if(this.isShow3d02 === 1) {
+      this.data3dList.push(this.data3d2);
+    } else {
+      this.data3dList.pop();
     }
   }
 
-  makePerspectiveOriginOut(index: string) {
-    if(index === '1') {
-      this.selectTransform1.peroriginz = this.data3d.transOrigin.type === 'fangwei' ? `${this.data3d.transOrigin.xx} ${this.data3d.transOrigin.yy}` :
-      this.data3d.transOrigin.type === 'percentage' ? `${this.data3d.transOrigin.x}% ${this.data3d.transOrigin.y}%` :
-          `${this.data3d.transOrigin.x}px ${this.data3d.transOrigin.y}px`;
-      this.renderer.setStyle(this.Translate3doutRef1.nativeElement, 'perspectiveOrigin', this.selectTransform1.peroriginz);
+  make3DTransformOut1() {
+    for(var i=0;i<this.data3dList.length;i++) {
+      let dataItem = this.data3dList[i];
+      if(i === 0) {
+        this.selectTransform1.trans = '';
+        this.selectTransform1.trans = `translate3d(${this.getValue(dataItem.trans.x,0)}px,${this.getValue(dataItem.trans.y,0)}px ,${this.getValue(dataItem.trans.z,0)}px) ` +
+            `scale3d(${+this.getValue(dataItem.scal.x,2)},${+this.getValue(dataItem.scal.y,2)},${+this.getValue(dataItem.scal.y,2)}) ` +
+            `rotate3d(${this.getValue(dataItem.rotat.x,3)},${this.getValue(dataItem.rotat.y,3)},${this.getValue(dataItem.rotat.z,3)},${this.getValue(dataItem.rotat.deg,0)}deg)`;
+        this.renderer.setStyle(this.Translate3doutRef1.nativeElement, 'transform', this.selectTransform1.trans);
+      }
+      if(i === 1) {
+        this.selectTransform2.trans = '';
+        this.selectTransform2.trans = `translate3d(${this.getValue(dataItem.trans.x,0)}px,${this.getValue(dataItem.trans.y,0)}px ,${this.getValue(dataItem.trans.z,0)}px) ` +
+            `scale3d(${+this.getValue(dataItem.scal.x,2)},${+this.getValue(dataItem.scal.y,2)},${+this.getValue(dataItem.scal.y,2)}) ` +
+            `rotate3d(${this.getValue(dataItem.rotat.x,3)},${this.getValue(dataItem.rotat.y,3)},${this.getValue(dataItem.rotat.z,3)},${this.getValue(dataItem.rotat.deg,0)}deg)`;
+        this.renderer.setStyle(this.Translate3doutRef2.nativeElement, 'transform', this.selectTransform2.trans);
+      }
+    }
+    this.makePerspectiveOut();
+    this.makePerspectiveOriginOut();
+    this.makebackfaceVisibilityOut();
+    this.maketransformStyleOut();
+    this.maketransformOriginOut();
+  }
+
+  makePerspectiveOut() {
+    for(var i=0;i<this.data3dList.length;i++) {
+      if(i === 0) {
+        this.selectTransform1.perspec = this.data3dList[i].perspec;
+        this.renderer.setStyle(this.Translate3doutRef1.nativeElement, 'perspective', this.selectTransform1.perspec);
+      }
+      if(i === 1) {
+        this.selectTransform2.perspec = this.data3dList[i].perspec;
+        this.renderer.setStyle(this.Translate3doutRef2.nativeElement, 'perspective', this.selectTransform2.perspec);
+      }
     }
   }
 
-  makebackfaceVisibilityOut(index: string) {
-    if(index === '1') {
-      this.selectTransform1.visib = this.data3d.visib;
-      this.renderer.setStyle(this.Translate3doutRef1.nativeElement, 'backfaceVisibility', this.selectTransform1.visib);
+  makePerspectiveOriginOut() {
+    for(var i=0;i<this.data3dList.length;i++) {
+      let data3d = this.data3dList[i];
+      if(i === 0) {
+        this.selectTransform1.perspec = data3d.transOrigin.type === 'fangwei' ? `${data3d.transOrigin.xx} ${data3d.transOrigin.yy}` :
+            data3d.transOrigin.type === 'percentage' ? `${data3d.transOrigin.x}% ${data3d.transOrigin.y}%` :
+                `${data3d.transOrigin.x}px ${data3d.transOrigin.y}px`;
+        this.renderer.setStyle(this.Translate3doutRef1.nativeElement, 'perspectiveOrigin', this.selectTransform1.perspec);
+      }
+      if(i === 1) {
+        this.selectTransform2.perspec = data3d.transOrigin.type === 'fangwei' ? `${data3d.transOrigin.xx} ${data3d.transOrigin.yy}` :
+            data3d.transOrigin.type === 'percentage' ? `${data3d.transOrigin.x}% ${data3d.transOrigin.y}%` :
+                `${data3d.transOrigin.x}px ${data3d.transOrigin.y}px`;
+        this.renderer.setStyle(this.Translate3doutRef2.nativeElement, 'perspectiveOrigin', this.selectTransform2.perspec);
+      }
     }
   }
 
-  maketransformStyleOut(index: string) {
-    if(index === '1') {
-      this.selectTransform1.stylez = this.data3d.stytez;
-      this.renderer.setStyle(this.Translate3doutRef1.nativeElement, 'transformStyle', this.selectTransform1.stylez);
+  makebackfaceVisibilityOut() {
+    for(var i=0;i<this.data3dList.length;i++) {
+      if(i === 0) {
+        this.selectTransform1.visib = this.data3dList[i].visib;
+        this.renderer.setStyle(this.Translate3doutRef1.nativeElement, 'backfaceVisibility', this.selectTransform1.visib);
+      }
+      if(i === 1) {
+        this.selectTransform2.visib = this.data3dList[i].visib;
+        this.renderer.setStyle(this.Translate3doutRef2.nativeElement, 'backfaceVisibility', this.selectTransform2.visib);
+      }
     }
   }
 
-  maketransformOriginOut(index: string) {
-    if(index === '1') {
-      this.selectTransform1.originz = this.data3d.originz.type === 'fangwei' ? `${this.data3d.originz.xx} ${this.data3d.originz.yy}` :
-          this.data3d.originz.type === 'percentage' ? `${this.data3d.originz.x}% ${this.data3d.originz.y}%` :
-              `${this.data3d.originz.x}px ${this.data3d.originz.y}px`;
-      this.renderer.setStyle(this.Translate3doutRef1.nativeElement, 'transformOrigin', this.selectTransform1.originz);
+  maketransformStyleOut() {
+    for(var i=0;i<this.data3dList.length;i++) {
+      if(i === 0) {
+        this.selectTransform1.stylez = this.data3dList[i].stylez;
+        this.renderer.setStyle(this.Translate3doutRef1.nativeElement, 'transformStyle', this.selectTransform1.stylez);
+      }
+      if(i === 1) {
+        this.selectTransform2.stylez = this.data3dList[i].stylez;
+        this.renderer.setStyle(this.Translate3doutRef2.nativeElement, 'transformStyle', this.selectTransform2.stylez);
+      }
+    }
+  }
+
+  maketransformOriginOut() {
+    for(var i=0;i<this.data3dList.length;i++) {
+      let data3d = this.data3dList[i];
+      if(i === 0) {
+        this.selectTransform1.originz = data3d.originz.type === 'fangwei' ? `${data3d.originz.xx} ${data3d.originz.yy}` :
+            data3d.originz.type === 'percentage' ? `${data3d.originz.x}% ${data3d.originz.y}%` :
+                `${data3d.originz.x}px ${data3d.originz.y}px`;
+        this.renderer.setStyle(this.Translate3doutRef1.nativeElement, 'transformOrigin', this.selectTransform1.originz);
+      }
+      if(i === 1) {
+        this.selectTransform2.originz = data3d.originz.type === 'fangwei' ? `${data3d.originz.xx} ${data3d.originz.yy}` :
+            data3d.originz.type === 'percentage' ? `${data3d.originz.x}% ${data3d.originz.y}%` :
+                `${data3d.originz.x}px ${data3d.originz.y}px`;
+        this.renderer.setStyle(this.Translate3doutRef2.nativeElement, 'transformOrigin', this.selectTransform2.originz);
+      }
     }
   }
 
