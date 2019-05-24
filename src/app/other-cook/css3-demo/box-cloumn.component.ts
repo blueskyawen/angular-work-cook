601672e36ccfc710dbe40bc770b882934a2ab03d
blueskyawen <bluesky.liu@163.com>
2019-05-25 01:22:46 +0800

    add columns

--- src/app/other-cook/css3-demo/box-cloumn.component.ts
+++ src/app/other-cook/css3-demo/box-cloumn.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2, ViewChild  } from '@angular/core';
 
 @Component({
   selector: 'app-box-cloumn',
@@ -6,10 +6,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./box-cloumn.component.less']
 })
 export class BoxCloumnComponent implements OnInit {
+  @ViewChild('columnh2')
+    Columnh2Ref: ElementRef;
+  colomnStr1: string = '';
+  @ViewChild('columnh3')
+    Columnh3Ref: ElementRef;
+  colomnStr2: string = '';
+  @ViewChild('columnsss')
+    ColumnsssRef: ElementRef;
+  colomnStr3: string = '';
 
-  constructor() { }
+  data: any = {
+    count: 3,grap:20,rule:{color:'#ccc',stylez:'solid',widthz:5},colspan:'all'
+  };
+  spanOptions: any[] = [
+    {label: '1',value: '1',disable: false},
+    {label: 'all',value: 'all',disable: false}
+  ];
+  styleOptions: any[] = [
+    {label: 'none',value: 'none',disable: false},
+    {label: 'hidden',value: 'hidden',disable: false},
+    {label: 'dotted',value: 'dotted',disable: false},
+    {label: 'dashed',value: 'dashed',disable: false},
+    {label: 'solid',value: 'solid',disable: false},
+    {label: 'double',value: 'double',disable: false},
+    {label: 'groove',value: 'groove',disable: false},
+    {label: 'ridge',value: 'ridge',disable: false},
+    {label: 'inset',value: 'inset',disable: false},
+    {label: 'outset',value: 'outset',disable: false}
+  ];
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit() {
+  }
+
+  makecolumnOut() {
+    this.colomnStr2 = this.data.colspan;
+    this.renderer.setStyle(this.Columnh3Ref.nativeElement, 'columnSpan', this.colomnStr2);
+    this.colomnStr3 = `${this.data.rule.widthz}px ${this.data.rule.stylez} ${this.data.rule.color}`;
+    this.renderer.setStyle(this.ColumnsssRef.nativeElement, 'columnRule', this.colomnStr3);
+    this.renderer.setStyle(this.ColumnsssRef.nativeElement, 'columnCount', this.data.count);
+    this.renderer.setStyle(this.ColumnsssRef.nativeElement, 'columnGap', `${this.data.grap}px`);
   }
 
 }
