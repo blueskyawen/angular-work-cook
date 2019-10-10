4adbba37da432f1dae0bd9c8836754308251dd1f
blueskyawen <bluesky.liu@163.com>
2019-10-10 20:33:42 +0800

    add

--- src/app/AppReuseStrategy.ts
+++ src/app/AppReuseStrategy.ts
@@ -1,20 +1,35 @@
 import { RouteReuseStrategy, ActivatedRouteSnapshot, DetachedRouteHandle } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
 
-export class AppReuseStrategy implements RouteReuseStrategy {
+export class RouteMsg {
+    url: string = '';
+    type: string = '';
+    constructor(type: string, url: string) {
+        this.type = type;
+        this.url = url;
+    }
+}
+
+export class AppRoutingCache implements RouteReuseStrategy {
 
     public static handlers: { [key: string]: DetachedRouteHandle } = {};
+    public static routeText$ = new Subject<RouteMsg>();
 
     public static deleteRouteSnapshot(path: string): void {
         const name = path.replace(/\//g, '_');
-        if (AppReuseStrategy.handlers[name]) {
-            delete AppReuseStrategy.handlers[name];
+        if (AppRoutingCache.handlers[name]) {
+            delete AppRoutingCache.handlers[name];
         }
     }
 
     public static clear(): void {
-        for (let key in AppReuseStrategy.handlers) {
-            delete AppReuseStrategy.handlers[key];
+        for (let key in AppRoutingCache.handlers) {
+            delete AppRoutingCache.handlers[key];
         }
+    }
+
+    public static getRouteText(): Observable<RouteMsg> {
+        return AppRoutingCache.routeText$.asObservable();
     }
 
     /** 当路由离开时会触发,表示对所有路由允许复用 如果你有路由不想利用可以在这加一些业务逻辑判断 */
@@ -23,31 +38,32 @@ export class AppReuseStrategy implements RouteReuseStrategy {
         if (!route.data.keep) {
             return false;
         }
+        AppRoutingCache.routeText$.next(new RouteMsg('detach', route['_routerState'].url));
         return true;
     }
 
     /** 当路由离开时会触发。按path作为key存储路由快照&组件当前实例对象 */
     store(route: ActivatedRouteSnapshot, handle: DetachedRouteHandle): void {
         console.debug('store======>', route, handle);
-        AppReuseStrategy.handlers[this.getRouteUrl(route)] = handle;
+        AppRoutingCache.handlers[this.getRouteUrl(route)] = handle;
     }
 
     /** 若 path 在缓存中有的都认为允许还原路由 */
     shouldAttach(route: ActivatedRouteSnapshot): boolean {
         console.debug('shouldAttach======>', route);
         console.debug('diffUrl======>', this.getRouteUrl(route));
-        return (route.data.keepParent || !route.routeConfig.children && !route.routeConfig.loadChildren) && !!AppReuseStrategy.handlers[this.getRouteUrl(route)];
+        return (route.data.keepParent || !route.routeConfig.children && !route.routeConfig.loadChildren) && !!AppRoutingCache.handlers[this.getRouteUrl(route)];
     }
 
     /** 从缓存中获取快照，若无则返回nul */
     retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle {
         console.debug('retrieve======>', route);
         console.debug('diffUrl======>', this.getRouteUrl(route));
-        if ((!route.data.keepParent && (route.routeConfig.children || route.routeConfig.loadChildren)) || !AppReuseStrategy.handlers[this.getRouteUrl(route)]) {
+        if ((!route.data.keepParent && (route.routeConfig.children || route.routeConfig.loadChildren)) || !AppRoutingCache.handlers[this.getRouteUrl(route)]) {
             return null;
         }
-
-        return AppReuseStrategy.handlers[this.getRouteUrl(route)];
+        AppRoutingCache.routeText$.next(new RouteMsg('attach', route['_routerState'].url));
+        return AppRoutingCache.handlers[this.getRouteUrl(route)];
     }
 
     /** 进入路由触发，判断是否同一路由 */
@@ -55,7 +71,7 @@ export class AppReuseStrategy implements RouteReuseStrategy {
         console.debug('shouldReuseRoute======>');
         console.debug('future======>', future);
         console.debug('curr======>', curr);
-        return (curr.data.keepParent || future.data.keepParent) || (future.routeConfig === curr.routeConfig && JSON.stringify(future.params) === JSON.stringify(curr.params));
+        return (!curr.data.keepParent || !future.data.keepParent) && (future.routeConfig === curr.routeConfig && JSON.stringify(future.params) === JSON.stringify(curr.params));
     }
 
     /** 使用route的path作为快照的key */
